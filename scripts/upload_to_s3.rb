73593e541395e6d4e2c322e63cddb8c00a8bd7cd
rushi <rushi@migrocer.in>
2020-02-05 16:31:24 +0530

    testing

--- scripts/upload_to_s3.rb
+++ scripts/upload_to_s3.rb
@@ -9,7 +9,7 @@ req.body = {
     build_id: ENV.fetch('TRAVIS_BUILD_ID'),
     commit: ENV.fetch('TRAVIS_COMMIT'),
     os: ENV.fetch('TRAVIS_OS_NAME'),
-}
+}.to_json
 
 res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
     http.request(req)
@@ -21,14 +21,15 @@ uri = URI(presigned_post['url'])
 request = Net::HTTP::Post.new(uri)
 form_data = []
 presigned_post['fields'].each do |key, value|
-    form_data.append([key, value])
+    form_data.push([key, value])
 end
 
 
 
-apk_path = "#{ENV.fetch('TRAVIS_BUILD_DIR')}/build/app/outputs/apk/release/app-release.apk"
+# apk_path = "#{ENV.fetch('TRAVIS_BUILD_DIR')}/build/app/outputs/apk/release/app-release.apk"
+apk_path = "../elastic_search.js"
 puts "Uploading apk from #{apk_path}"
-form_data.append(['file', File.open(apk_path)])
+form_data.push(['file', File.open(apk_path)])
 request.set_form form_data, 'multipart/form-data'
 
 response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http| # pay attention to use_ssl if you need it
